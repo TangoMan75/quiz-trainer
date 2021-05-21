119f7205a658a069bcb872704e396f6e6c252ae2
Matthias Morin <14804833+TangoMan75@users.noreply.github.com>
2021-05-21 16:16:59 +0200

    2021-05-21 16:16:59

---
+++ precache-manifest.a53789f8724d558bdcf1f74bbbf5d990.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/quiz-trainer/fonts/glyphicons-halflings-regular.eot"
+  },
+  {
+    "revision": "a15c2ac3234aa8f6064ef9c1f7383c37",
+    "url": "/quiz-trainer/css/bootstrap.min.css"
+  },
+  {
+    "revision": "e255fd12316f5b051e51",
+    "url": "/quiz-trainer/js/chunk-vendors.4e6b9ca4.js"
+  },
+  {
+    "revision": "25766ad54e57f6448055b4ae7582a52c",
+    "url": "/quiz-trainer/img/logo.25766ad5.png"
+  },
+  {
+    "revision": "06d4a5ea2b7d45be9eea5e971508bb0b",
+    "url": "/quiz-trainer/index.html"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/quiz-trainer/robots.txt"
+  },
+  {
+    "revision": "794d8d7ee828459ca8f52929cdb94a15",
+    "url": "/quiz-trainer/css/glyphicons.min.css"
+  },
+  {
+    "revision": "9015a1a2c82ec76695fe",
+    "url": "/quiz-trainer/js/app.3d8b9384.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/quiz-trainer/fonts/glyphicons-halflings-regular.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/quiz-trainer/fonts/glyphicons-halflings-regular.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/quiz-trainer/fonts/glyphicons-halflings-regular.ttf"
+  },
+  {
+    "revision": "e1d98d47689e00f8ecbc5d9f61bdb42e",
+    "url": "/quiz-trainer/js/bootstrap.min.js"
+  },
+  {
+    "revision": "ac0e50590d92259238b57ba6a74ed568",
+    "url": "/quiz-trainer/js/jquery-3.3.1.slim.min.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/quiz-trainer/fonts/glyphicons-halflings-regular.svg"
+  },
+  {
+    "revision": "9015a1a2c82ec76695fe",
+    "url": "/quiz-trainer/css/app.07540ee2.css"
+  }
+];
